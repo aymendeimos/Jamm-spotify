ee6c507abfba53a9244aef1b3280179a6c7509a4
aymendeimos <aymengraoui@gmail.com>
2018-06-06 16:08:05 +0000

    Add methodes to save search term in localstorage

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -5,14 +5,18 @@ class SearchBar extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            term: ''
+            term: this.getTerm()
         }
         this.search = this.search.bind(this);
         this.handleTermChange = this.handleTermChange.bind(this);
         this.handleKeyPress = this.handleKeyPress.bind(this);
+        this.getTerm = this.getTerm.bind(this);
+        this.setLocalStorageTerm = this.setLocalStorageTerm.bind(this)
+        
     }
 
     search(){
+        this.setLocalStorageTerm();
         this.props.onSearch(this.state.term);
     }
 
@@ -21,6 +25,8 @@ class SearchBar extends React.Component{
         this.setState({
             term: term
         })
+
+        this.setLocalStorageTerm();
     }
 
     handleKeyPress(event) {
@@ -29,10 +35,23 @@ class SearchBar extends React.Component{
         }
     }
 
+    setLocalStorageTerm(){
+        localStorage.setItem('term',JSON.stringify({
+            term: this.state.term
+        }))
+    }
+
+    getTerm(){
+        let localstorageTerm = JSON.parse(localStorage.getItem('term'));
+        if(localstorageTerm){
+            return localstorageTerm.term;
+        }
+    }
+
     render(){
         return(
             <div className="SearchBar">
-                <input placeholder="Enter A Song, Album, or Artist" onChange={this.handleTermChange} onKeyPress={this.handleKeyPress}/>
+                <input placeholder="Enter A Song, Album, or Artist" value={this.state.term} onChange={this.handleTermChange} onKeyPress={this.handleKeyPress}/>
                 <a onClick={this.search}>SEARCH</a>
             </div>
         )
